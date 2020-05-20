bfd26b55f8a8e307ff200313d358cf45f2f08765
sht3898 <sehyunteg@naver.com>
2020-05-20 09:27:53 +0900

    S02P31B105-48 | 200520 | 셀리니움 활용법 익히기 동적 크롤링을 위한 셀리니움의 활용법을 익혀서 크롤링은 한다

--- parsing.py
+++ parsing.py
@@ -1,10 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
+# headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
+# url = "https://www.frip.co.kr/products/108923"
+# webpage = requests.get(url, headers=headers)
+# soup = BeautifulSoup(webpage.content, "html.parser").body.text
+# print(soup)
 
+from selenium import webdriver
 
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
-url = "https://www.frip.co.kr/products/108923"
-webpage = requests.get(url, headers=headers)
-soup = BeautifulSoup(webpage.content, "html.parser")
-
-print(soup)
+driver = webdriver.Chrome('./chromedriver')
+driver.get('https://www.frip.co.kr/daily/learning/hot?page=1')
