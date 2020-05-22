d1365766af403a27bc613ef894399b2665440e2f
gaeunek <l9960204@naver.com>
2020-05-22 13:35:24 +0900

    S02P31B105-57 | 200522 | Search 관련 기능 추가 Search 관련 기능 추가 중 MySql에서 FULLTEXT 사용을 위한 설정 완료 Meeting Entity 수정

--- Backend/jpa/wiselife/src/main/java/com/ssafy/wiselife/service/MeetingServiceImpl.java
+++ Backend/jpa/wiselife/src/main/java/com/ssafy/wiselife/service/MeetingServiceImpl.java
@@ -13,8 +13,11 @@ import com.ssafy.wiselife.domain.UserMeeting;
 import com.ssafy.wiselife.dto.MeetingDTO.CreateMeeting;
 import com.ssafy.wiselife.dto.MeetingDTO.DetailMeeting;
 import com.ssafy.wiselife.dto.MeetingDTO.UpdateMeeting;
+import com.ssafy.wiselife.mapper.EntityMapper;
+import com.ssafy.wiselife.repository.CategoryRepository;
 import com.ssafy.wiselife.repository.MeetingRepository;
 import com.ssafy.wiselife.repository.UserMeetingRepository;
+import com.ssafy.wiselife.repository.UserRepository;
 
 @Service
 public class MeetingServiceImpl implements IMeetingService {
@@ -24,43 +27,138 @@ public class MeetingServiceImpl implements IMeetingService {
 
 	@Autowired
 	private UserMeetingRepository usermeetingrepo;
+	
+	@Autowired
+	private CategoryRepository categoryrepo;
+	
+	@Autowired
+	private UserRepository userrepo;
 
 	@Autowired
 	private ModelMapper modelMapper; // DTO -> Entity
-
+	
+	@Autowired
+	private EntityMapper entityMapper;
+	
 	@Override
-	public int createMeeting(CreateMeeting meeting) {
+	public int createMeeting(long uid, CreateMeeting meeting) {
+		//meetingDate 프런트랑 얘기해보기
 		try {
-			User user = new User();
-			user.setUid(meeting.getUid());
+			User user = userrepo.findById(uid).get();
 			Category category = new Category();
-			category.setCategoryId(meeting.getMainCategory());
+			try {
+				category = categoryrepo.findById(meeting.getMainCategory()).get();
+			} catch (Exception e) {
+				return -2;
+			}
+			
 			Meeting meetingEntity = modelMapper.map(meeting, Meeting.class);
 			Date date = new Date();
 			meetingEntity.setCreatedAt(date);
 			meetingEntity.setUpdatedAt(date);
 			meetingEntity.setNowPerson(1);
+			meetingEntity.setUser(user);
+			meetingEntity.setCategory(category);
+			
 			meetingrepo.save(meetingEntity);
-			return 1;
+			
+			int meeting_id = meetingrepo.findLaseMeetingId();
+			Meeting findMeeting = meetingrepo.findById(meeting_id).get();
+			UserMeeting usermeeting = new UserMeeting();
+			usermeeting.setMeeting(findMeeting);
+			usermeeting.setUser(meetingEntity.getUser());
+			usermeetingrepo.save(usermeeting);
+			
+			return meeting_id;
 		} catch (Exception e) {
 			return -1;
 		}
 	}
 
 	@Override
-	public void updateMeeting(UpdateMeeting meeting) {
-
+	public int updateMeeting(int meeting_id, long uid, UpdateMeeting meeting) {
+		User user = new User();
+		try {
+			user = userrepo.findById(uid).get();
+			meetingrepo.findByMeetingIdAndUser(meeting_id, user);
+		} catch (Exception e) {
+			return 0;
+		}
+		
+		try {
+			Category category = new Category();
+			
+			try {
+				category = categoryrepo.findById(meeting.getMainCategory()).get();
+			} catch (Exception e) {
+				return -2;
+			}
+		
+			Meeting meetingEntity = meetingrepo.findById(meeting_id).get();
+			meetingEntity.setUpdatedAt(new Date());
+			meetingEntity.setUser(user);
+			meetingEntity.setCategory(category);
+			meetingEntity.setMeetingId(meeting_id);
+			meetingEntity.setIsPeriod(meeting.getIsPeriod());
+			meetingEntity.setMeetingDate(meeting.getMeetingDate());
+			meetingEntity.setPeriodDate(meeting.getPeriodDate());
+			meetingEntity.setIsClass(meeting.getIsClass());
+			meetingEntity.setContent(meeting.getContent());
+			meetingEntity.setRefUrl(meeting.getRefUrl());
+			meetingEntity.setAddress(meeting.getAddress());
+			meetingEntity.setFee(meeting.getFee());
+			meetingEntity.setUnit(meeting.getUnit());
+			meetingEntity.setIsActive(meeting.getIsActive());
+			meetingEntity.setTags(meeting.getTags());
+			meetingrepo.save(meetingEntity);
+			return meeting_id;
+		} catch (Exception e) {
+			return -1;
+		}
 	}
 
 	@Override
 	public DetailMeeting detailMeeting(int meeting_id) {
-		// TODO Auto-generated method stub
-		return null;
+		//리뷰도 같이 보여줘야함
+		try {
+			DetailMeeting meeting = entityMapper.convertToDomain(meetingrepo.findById(meeting_id).get(), DetailMeeting.class);
+			return meeting;
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 	@Override
-	public int findByLastMeetingId() {
-		return meetingrepo.findMeetingId();
+	public boolean checkAuthentication(long uid) {
+		try {
+			userrepo.findById(uid).get();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
+	@Override
+	public int deleteMeeting(int meeting_id, long uid) {
+		Meeting meeting = new Meeting();
+		try {
+			meeting = meetingrepo.findById(meeting_id).get();
+		} catch (Exception e) {
+			return -2;
+		}
+		
+		try {
+			User user = userrepo.findById(uid).get();
+			meetingrepo.findByMeetingIdAndUser(meeting_id, user);
+		} catch (Exception e) {
+			return 0;
+		}
+
+		try {
+			meetingrepo.delete(meeting);
+			return 1;
+		} catch (Exception e) {
+			return -1;
+		}
+	}
 }
---
+++ Backend/jpa/wiselife/src/main/java/com/ssafy/wiselife/service/SearchServiceImpl.java
@@ -0,0 +1,55 @@
+package com.ssafy.wiselife.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.wiselife.domain.Meeting;
+import com.ssafy.wiselife.dto.MeetingDTO.CardMeeting;
+import com.ssafy.wiselife.mapper.EntityMapper;
+import com.ssafy.wiselife.repository.CategoryRepository;
+import com.ssafy.wiselife.repository.MeetingRepository;
+
+@Service
+public class SearchServiceImpl implements ISearchService {
+	@Autowired
+	private CategoryRepository categoryrepo;
+	
+	@Autowired
+	private MeetingRepository meetingrepo;
+	
+	@Autowired
+	private EntityMapper entityMapper;
+
+	@Override
+	public List<CardMeeting> searchKeyword(int category_id, String keyword) {
+		try {
+			//메인페이지에서 검색
+			List<String> meetingList = null;
+			if(category_id == 0 || categoryrepo.findById(category_id).get() != null) {
+				if(keyword.contains("#")) { //해시태그 검색
+					String[] tags = keyword.split(" ");
+					String keywords = "";
+					for (int i = 0; i < tags.length; i++) {
+						keywords += tags[i].substring(1, tags[i].length()) + " ";
+					}
+					
+					keywords = keywords.substring(0,keyword.length()-1);
+					System.out.println(keywords);
+//					meetingrepo.alterTable();
+					meetingList = meetingrepo.findByTags(keyword);
+					System.out.println(meetingList.toString());
+					return meetingList.stream().map(e -> entityMapper.convertToDomain(e, CardMeeting.class)).collect(Collectors.toList());
+				}
+			}
+		} catch (Exception e) {
+			return null;
+		}
+		
+		return null;
+	}
+	
+
+}
