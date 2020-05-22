747a4be22270d73cbfbc915b9df0ad90dc4678d2
Daerae_Kim <drk0830@naver.com>
2020-05-22 15:34:03 +0900

    S02P31B105-9 | 200522 | 카카오 로그인 구현 중 카카오 로그인 구현, Back-Front 연동 중 CORS 오류 발생

--- Frontend/src/http-common.js
+++ Frontend/src/http-common.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export default axios.create({
 //  baseURL : "http://i02b206.p.ssafy.io/dc/" // AWS
- baseURL: "http://localhost:8000/wiselife/" // 로컬
+//  baseURL: "http://localhost:8000/wiselife/" // 로컬
+ baseURL: "http://172.30.1.36:8080/api/" // 로컬
 });
--- Frontend/src/views/LoginPage.vue
+++ Frontend/src/views/LoginPage.vue
@@ -4,95 +4,120 @@
       <v-card class="mx-auto" max-width="75%">
         <v-row>
           <v-col class="hidden-sm-and-down">
-            <img-banner :imgSrc="bannerImg"/>
+            <img-banner :imgSrc="bannerImg" />
           </v-col>
           <v-col>
             <div style="padding:50px;">
-            <v-img src="../assets/wiselife.png" style="margin:auto;" height="110px" width="170px"></v-img>
+              <v-img src="../assets/wiselife.png" style="margin:auto;" height="110px" width="170px"></v-img>
             </div>
             <v-spacer></v-spacer>
             <div style="padding:50px;">
-            <h2 style="text-align:center;">빅데이터 기반으로<br/>당신의 맞춤 여가생활을<br/>추천해드립니다!</h2>
+              <h2 style="text-align:center;">
+                빅데이터 기반으로
+                <br />당신의 맞춤 여가생활을
+                <br />추천해드립니다!
+              </h2>
             </div>
             <v-spacer></v-spacer>
             <div style="padding:50px;">
-            <v-img
-              style="cursor:pointer; margin:auto"
-              max-width="450px"
-              src="../assets/kakao_login_btn_large_wide.png"
-              @click="login()"
-            ></v-img>
+              <v-img
+                style="cursor:pointer; margin:auto"
+                max-width="450px"
+                src="../assets/kakao_login_btn_large_wide.png"
+                @click="login()"
+              ></v-img>
             </div>
           </v-col>
         </v-row>
       </v-card>
     </div>
     <sign-up v-if="!isMember" :token="token" />
-    <!-- <KakaoLogin
-      api-key="c2d9f09a902e77b8550b754cdb90d407"
-      image="kakao_login_btn_large"
-      :on-success=onSuccess
-      :on-failure=onFailure
-    /> -->
   </v-container>
-  
 </template>
 
 <script>
 import http from "../http-common";
 import SignUp from "@/components/login/SignUp.vue";
 import ImgBanner from "@/components/login/ImgBanner";
-// import KakaoLogin from 'vue-kakao-login'
 
-let onSuccess = (data) => {
-  console.log(data)
-  console.log("success")
-}
-let onFailure = (data) => {
-  console.log(data)
-  console.log("failure")
-}
+Kakao.init("c2d9f09a902e77b8550b754cdb90d407");
 
 export default {
   name: "loginPage",
   components: {
     SignUp,
-    ImgBanner,
-    // KakaoLogin
+    ImgBanner
   },
   data() {
     return {
       isMember: true,
       clickBtn: false,
       token: "",
-      bannerImg: [this.getImgUrl("banner1.jpg"), this.getImgUrl("banner2.jpg")],
+      bannerImg: [this.getImgUrl("banner1.jpg"), this.getImgUrl("banner2.jpg")]
     };
   },
-  mounted(){
-    if(sessionStorage.getItem('token')){
-      this.$router.push('/');
+  mounted() {
+    if (sessionStorage.getItem("token")) {
+      this.$router.push("/");
     }
   },
   methods: {
-    onSuccess,
-    onFailure,
     login() {
       this.clickBtn = true;
-      let token = "asdfasdfasdfasdfasdf";
-      let username="대래";
-      let status = 2;
-      // status { 1 : login ok, member ok / 2 : login ok, member : no / 3 : error }
-      if (status == 1) {
-        sessionStorage.setItem("token", token);
-        sessionStorage.setItem("username", username);
-        this.$router.go();
-      } else if (status == 2) {
-        sessionStorage.setItem("username", username);
-        this.token = token;
-        this.isMember = false;
-      } else {
-        this.clickBtn = false;
-      }
+      Kakao.Auth.login({
+        success: function(authObj) {
+          console.log(authObj);
+          let headers = {
+            access_token: authObj.access_token,
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type":"application/json",
+          };
+          http
+            .post(`user/login`, null, {
+              headers
+            })
+            .then(response => {
+              if (response.data.state) {
+                var username = "";
+                Kakao.API.request({
+                  url: "/v2/user/me",
+                  success: function(response) {
+                    username = response.properties.nickname;
+                  },
+                  fail: function(error) {
+                    alert("회원 정보를 가져오는데 실패했습니다!");
+                  }
+                });
+                sessionStorage.setItem("token", access_token);
+                sessionStorage.setItem("username", username);
+              } else {
+                this.token = access_token;
+                this.isMember = false;
+              }
+            });
+        },
+        fail: function(err) {
+          console.log(err);
+          alert("로그인에 실패하였습니다! 다시 시도해주세요!");
+          return;
+        }
+      });
+      // this.clickBtn = true;
+      // let token = "asdfasdfasdfasdfasdf";
+      // let username = "대래";
+      // let status = 2;
+      // // status { 1 : login ok, member ok / 2 : login ok, member : no / 3 : error }
+      // if (status == 1) {
+      //   sessionStorage.setItem("token", token);
+      //   sessionStorage.setItem("username", username);
+      //   this.$router.go();
+      // } else if (status == 2) {
+      //   sessionStorage.setItem("username", username);
+      //   this.token = token;
+      //   this.isMember = false;
+      // } else {
+      //   this.clickBtn = false;
+      // }
       //   http.get(`kakaoLogin/`).then(res => {
       //     if (res.data.state == 200) {
 
@@ -101,7 +126,7 @@ export default {
     },
     getImgUrl(img) {
       return require("../assets/" + img);
-    },
+    }
   }
 };
 </script>
