0588737d5b8559ad7fb46ec211a53d503c0cb0ca
안지연 <jennyahn94@naver.com>
2020-05-25 15:35:44 +0900

    S02P31B105-55 | 200525 | 강좌/모임 상세보기 구현 강좌/모임 상세보기 페이지 구현 - 리뷰 탭 구현

---
+++ Frontend/src/components/tabs/Review.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container class="ma-auto">
+    <v-card outlined class="pa-3 mb-2" width="100%" height="100%">
+      <v-row>
+        <v-col style="text-align:right">
+          <span class="toptitle">평균 평점</span>
+          <span class="topscore ml-2">3.2</span>
+        </v-col>
+        <v-col>
+          <v-rating
+            :value="3.2"
+            background-color="white"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            medium
+          ></v-rating>
+          
+        </v-col>
+        <v-col cols="5">
+          <span class="toptitle">참여자 평균 연령대</span>
+          <span class="topscore ml-2">20대</span>
+        </v-col>
+      </v-row>
+
+         
+    </v-card>
+     <review-content></review-content>
+  </v-container>
+</template>
+<script>
+import ReviewContent from "./ReviewContent"
+export default {
+    name:"review",
+    components:{
+        ReviewContent
+    }
+};
+</script>
+<style scoped>
+.toptitle {
+  font-size: 12pt;
+}
+@import url("https://fonts.googleapis.com/css2?family=Jua&display=swap");
+.topscore {
+  font-size: 15pt;
+  font-family: "Jua", sans-serif;
+}
+</style>
