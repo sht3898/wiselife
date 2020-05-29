82e8ee1f1250727c98cdd9beb8014c214b51fe79
김가은 <l9960204@naver.com>
2020-05-29 15:13:12 +0900

    S02P31B105-66 | 200529 | LikeMeeting 관계 수정

--- Backend/wiselife/src/main/java/com/ssafy/wiselife/domain/User.java
+++ Backend/wiselife/src/main/java/com/ssafy/wiselife/domain/User.java
@@ -3,12 +3,9 @@ package com.ssafy.wiselife.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.OnDelete;
@@ -28,41 +25,45 @@ import lombok.ToString;
 public class User {
 	@Id
 	private long uid;
-	
+
 	private String username;
-	
+
 	private String profileImage;
-	
+
 	private int isInst;
-	
+
 	private int gender;
-	
+
 	private int year;
-	
+
 	private int ages;
-	
+
 	private String area1;
-	
+
 	private String area2;
-	
+
 	@OneToMany(mappedBy = "user")
 	@OnDelete(action = OnDeleteAction.CASCADE)
 	@JsonIgnore
-	private List<InterestCategory> userCategories = new ArrayList<>();
-	
+	private List<InterestCategory> interestCategoryList = new ArrayList<>();
+
 	@OneToMany(mappedBy = "user")
 	@OnDelete(action = OnDeleteAction.CASCADE)
 	@JsonIgnore
-	private List<UserMeeting> userMeetings = new ArrayList<>();
-	
+	private List<UserMeeting> userMeetingList = new ArrayList<>();
+
 	@OneToMany(mappedBy = "user")
 	@OnDelete(action = OnDeleteAction.CASCADE)
 	@JsonIgnore
 	private List<Review> reviewList = new ArrayList<>();
-	
+
 	@OneToMany(mappedBy = "user")
 	@OnDelete(action = OnDeleteAction.CASCADE)
 	@JsonIgnore
 	private List<Meeting> meetingList = new ArrayList<>();
-		
+
+	@OneToMany(mappedBy = "user")
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonIgnore
+	private List<LikeMeeting> likeMeetingList = new ArrayList<>();
 }
