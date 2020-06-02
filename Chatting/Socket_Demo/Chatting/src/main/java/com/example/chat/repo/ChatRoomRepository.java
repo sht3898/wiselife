de3157b2f78b97d3b254d08444dad042673c2efb
soobin1080 <soobin1080@naver.com>
2020-06-02 16:40:42 +0900

    S02P31B105 | 200602 | 채팅 오류 해결 X

--- Chatting/Socket_Demo/Chatting/src/main/java/com/example/chat/repo/ChatRoomRepository.java
+++ Chatting/Socket_Demo/Chatting/src/main/java/com/example/chat/repo/ChatRoomRepository.java
@@ -18,31 +18,27 @@ import com.example.chat.model.ChatRoom;
 @Repository
 public class ChatRoomRepository {
 
-	 private Map<String, ChatRoom> chatRoomMap;
-	    @PostConstruct
-	    private void init() {
-	        chatRoomMap = new LinkedHashMap<>();
-	    }
-	    public List<ChatRoom> findAllRoom() {
-	        // 채팅방 생성순서 최근 순으로 반환
-	        List<ChatRoom> chatRooms = new ArrayList<>(chatRoomMap.values());
-	        Collections.reverse(chatRooms);
-	        return chatRooms;
-	    }
-	    public ChatRoom findRoomById(String id) {
-	        return chatRoomMap.get(id);
-	    }
-	    public ChatRoom createChatRoom(String name) {
-	    	ChatRoom chatRoom = ChatRoom.create(name);
-	    	try {
-		        chatRoomMap.put(chatRoom.getRoomId(), chatRoom);
-		        return chatRoom;
-				
-			} catch (Exception e) {
-				// TODO: handle exception
-				System.out.println("message:"+e.getMessage());
-				return chatRoom;
-			}
-	        
-	    }
+	private Map<String, ChatRoom> chatRoomMap;
+
+	@PostConstruct
+	private void init() {
+		chatRoomMap = new LinkedHashMap<>();
+	}
+
+	public List<ChatRoom> findAllRoom() {
+		// 채팅방 생성순서 최근 순으로 반환
+		List<ChatRoom> chatRooms = new ArrayList<>(chatRoomMap.values());
+		Collections.reverse(chatRooms);
+		return chatRooms;
+	}
+
+	public ChatRoom findRoomById(String id) {
+		return chatRoomMap.get(id);
+	}
+
+	public ChatRoom createChatRoom(String name) {
+		ChatRoom chatRoom = ChatRoom.create(name);
+		chatRoomMap.put(chatRoom.getRoomId(), chatRoom);
+		return chatRoom;
+	}
 }
