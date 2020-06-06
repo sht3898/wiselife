38d9891ef8a65895950a5e5ddc0b46bfc86f9b40
soobin1080 <soobin1080@naver.com>
2020-06-06 20:20:17 +0900

    t

--- Backend/wiselife/src/main/java/com/ssafy/wiselife/WiselifeApplication.java
+++ Backend/wiselife/src/main/java/com/ssafy/wiselife/WiselifeApplication.java
@@ -5,11 +5,14 @@ import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class WiselifeApplication extends SpringBootServletInitializer implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
