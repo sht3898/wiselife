a7d059b23044ff30e670b6379e14ee41817602ad
gaeunek <l9960204@naver.com>
2020-06-06 18:15:51 +0900

    S02P31B105-96 | 200606 | Review 수정 내가 작성한 리뷰 목록 확인하는 API 추가

--- Backend/wiselife/src/main/java/com/ssafy/wiselife/service/IReviewService.java
+++ Backend/wiselife/src/main/java/com/ssafy/wiselife/service/IReviewService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.ssafy.wiselife.dto.MeetingDTO.CheckMeetingOfReview;
 import com.ssafy.wiselife.dto.ReviewDTO.DetailReview;
+import com.ssafy.wiselife.dto.ReviewDTO.MyDetailReview;
 import com.ssafy.wiselife.dto.ReviewDTO.WriteReview;
 
 public interface IReviewService {
@@ -14,4 +15,6 @@ public interface IReviewService {
 	int deleteReview(int review_id, long uid);
 
 	List<DetailReview> showMeetingOfReviewList(int meeting_id);
+
+	List<MyDetailReview> getMyReview(long uid);
 }
